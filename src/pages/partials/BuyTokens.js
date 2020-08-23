d56e48bf20e24855f22a8dfd3bb146fae648f4d4
michelnogales <michelnogales@MacBook-Pro-de-michelnogales.local>
2020-08-23 15:34:27 -0500

    fix

--- src/pages/partials/BuyTokens.js
+++ src/pages/partials/BuyTokens.js
@@ -124,52 +124,7 @@ export function BuyTokens({placeholder, ownerStxAddress}) {
         }
     };
 
-    const handleSubmit2 = async (e) => {
-        e.preventDefault();
-        spinner.current.classList.remove('d-none');
 
-        var amountAsString = textfield.current.value.trim();
-        var amount = parseInt(amountAsString);
-
-        // 1 3
-        //let mounth = 1;
-
-        try {
-
-            setStatus(`Sending transaction`);
-
-
-
-            await doContractCall({
-                contractAddress: 'ST2R1XSFXYHCSFE426HP45TTD8ZWV9XHX2SRP3XA8',
-                contractName:  'prueba1000',
-                functionName: 'transfer-reverse-tokens',
-                functionArgs: [uintCV(amount)],
-                postConditionMode: PostConditionMode.Deny,
-                postConditions: [
-                    makeStandardSTXPostCondition(
-                        ownerStxAddress,
-                        FungibleConditionCode.LessEqual,
-                        new BigNum(amount)
-                    ),
-                ],
-                appDetails,
-                finished: data => {
-
-                    console.log("data setStatus:", data);
-
-                    setStatus(txIdToStatus(data.txId));
-                    spinner.current.classList.add('d-none');
-                },
-            });
-
-
-        } catch (e) {
-            console.log(e);
-            setStatus(e.toString());
-            spinner.current.classList.add('d-none');
-        }
-    };
 
     const onClick = async () => {
 
@@ -400,55 +355,6 @@ export function BuyTokens({placeholder, ownerStxAddress}) {
                     </Form>
 
 
-                    <h4>transfer-reverse-tokens</h4>
-
-                    <Form onSubmit={(e) => handleSubmit2(e)}>
-                        <Form.Group controlId="Deposit">
-                            <Form.Label>Deposit Amount</Form.Label>
-                            <InputGroup className="mb-3">
-                                <FormControl
-                                    aria-label="Recipient's Deposit"
-                                    aria-describedby="basic-addon2"
-                                    type="decimal"
-                                    ref={textfield}
-                                    className="form-control"
-                                    defaultValue={''}
-                                    placeholder={placeholder}
-                                />
-                                <InputGroup.Append>
-                                    <InputGroup.Text id="basic-addon2">STX</InputGroup.Text>
-                                </InputGroup.Append>
-                            </InputGroup>
-                        </Form.Group>
-
-
-                        <Form.Group controlId="Lockup">
-                            <Form.Label>Lockup Period</Form.Label>
-                            <Form.Control as="select">
-                                <option>3 Months</option>
-                                <option>6 Months</option>
-                                <option>9 Months</option>
-                            </Form.Control>
-                        </Form.Group>
-
-
-                        <Form.Group controlId="Currency">
-                            <Form.Label>Return Currency</Form.Label>
-                            <Form.Control as="select">
-                                <option>STX</option>
-                            </Form.Control>
-                        </Form.Group>
-
-                        <Button variant="primary" type="submit">
-                            <div
-                                ref={spinner}
-                                role="status"
-                                className="d-none spinner-border spinner-border-sm text-info align-text-top mr-2"
-                            />
-                            Get loan now
-                        </Button>
-
-                    </Form>
 
 
                     {status && (
