57e11f8c6538079cc1c6a6faf884cba5a370ef0b
michelnogales <michelnogales@MacBook-Pro-de-michelnogales.local>
2020-08-23 13:53:33 -0500

    add contract

--- src/pages/partials/BuyTokens.js
+++ src/pages/partials/BuyTokens.js
@@ -93,25 +93,70 @@ export function BuyTokens({placeholder, ownerStxAddress}) {
         try {
 
             setStatus(`Sending transaction`);
-            const stx = 1;
-            const months = 1;
-
 
             await doContractCall({
-                contractAddress: 'ST1618RD5WAQMGNX4KQ4KBWBGP0X59BT5P324DB1S',//'ST2R1XSFXYHCSFE426HP45TTD8ZWV9XHX2SRP3XA8',//CONTRACT_ADDRESS,
-                contractName:  'loan-test-15',//'transfer-tokens',//CONTRACT_NAME,
-                functionName: 'get-stx-return',//'transfer-tokens',//'buy-tokens',
-                functionArgs: [intCV(stx),intCV(months)],
+                contractAddress: 'ST2R1XSFXYHCSFE426HP45TTD8ZWV9XHX2SRP3XA8',
+                contractName:  'prueba1000',
+                functionName: 'transfer-tokens',
+                functionArgs: [uintCV(amount)],
                 postConditionMode: PostConditionMode.Deny,
                 postConditions: [
                     makeStandardSTXPostCondition(
                         ownerStxAddress,
                         FungibleConditionCode.LessEqual,
-                        new BigNum(stx)
+                        new BigNum(amount)
                     ),
                 ],
                 appDetails,
                 finished: data => {
+
+                    console.log("data setStatus:", data);
+                    setStatus(txIdToStatus(data.txId));
+                    spinner.current.classList.add('d-none');
+                },
+            });
+
+
+        } catch (e) {
+            console.log(e);
+            setStatus(e.toString());
+            spinner.current.classList.add('d-none');
+        }
+    };
+
+    const handleSubmit2 = async (e) => {
+        e.preventDefault();
+        spinner.current.classList.remove('d-none');
+
+        var amountAsString = textfield.current.value.trim();
+        var amount = parseInt(amountAsString);
+
+        // 1 3
+        //let mounth = 1;
+
+        try {
+
+            setStatus(`Sending transaction`);
+
+
+
+            await doContractCall({
+                contractAddress: 'ST2R1XSFXYHCSFE426HP45TTD8ZWV9XHX2SRP3XA8',
+                contractName:  'prueba1000',
+                functionName: 'transfer-reverse-tokens',
+                functionArgs: [uintCV(amount)],
+                postConditionMode: PostConditionMode.Deny,
+                postConditions: [
+                    makeStandardSTXPostCondition(
+                        ownerStxAddress,
+                        FungibleConditionCode.LessEqual,
+                        new BigNum(amount)
+                    ),
+                ],
+                appDetails,
+                finished: data => {
+
+                    console.log("data setStatus:", data);
 
                     setStatus(txIdToStatus(data.txId));
                     spinner.current.classList.add('d-none');
@@ -305,7 +350,7 @@ export function BuyTokens({placeholder, ownerStxAddress}) {
                 <Card.Header>Please fill form to receive 5% STX per month on your deposit</Card.Header>
                 <Card.Body>
 
-
+                    <h4>transfer-tokens</h4>
                     <Form onSubmit={(e) => handleSubmit(e)}>
                         <Form.Group controlId="Deposit">
                             <Form.Label>Deposit Amount</Form.Label>
@@ -354,6 +399,58 @@ export function BuyTokens({placeholder, ownerStxAddress}) {
 
                     </Form>
 
+
+                    <h4>transfer-reverse-tokens</h4>
+
+                    <Form onSubmit={(e) => handleSubmit2(e)}>
+                        <Form.Group controlId="Deposit">
+                            <Form.Label>Deposit Amount</Form.Label>
+                            <InputGroup className="mb-3">
+                                <FormControl
+                                    aria-label="Recipient's Deposit"
+                                    aria-describedby="basic-addon2"
+                                    type="decimal"
+                                    ref={textfield}
+                                    className="form-control"
+                                    defaultValue={''}
+                                    placeholder={placeholder}
+                                />
+                                <InputGroup.Append>
+                                    <InputGroup.Text id="basic-addon2">STX</InputGroup.Text>
+                                </InputGroup.Append>
+                            </InputGroup>
+                        </Form.Group>
+
+
+                        <Form.Group controlId="Lockup">
+                            <Form.Label>Lockup Period</Form.Label>
+                            <Form.Control as="select">
+                                <option>3 Months</option>
+                                <option>6 Months</option>
+                                <option>9 Months</option>
+                            </Form.Control>
+                        </Form.Group>
+
+
+                        <Form.Group controlId="Currency">
+                            <Form.Label>Return Currency</Form.Label>
+                            <Form.Control as="select">
+                                <option>STX</option>
+                            </Form.Control>
+                        </Form.Group>
+
+                        <Button variant="primary" type="submit">
+                            <div
+                                ref={spinner}
+                                role="status"
+                                className="d-none spinner-border spinner-border-sm text-info align-text-top mr-2"
+                            />
+                            Get loan now
+                        </Button>
+
+                    </Form>
+
+
                     {status && (
                         <>
                             <div>{status}</div>
@@ -361,16 +458,6 @@ export function BuyTokens({placeholder, ownerStxAddress}) {
                     )}
                 </Card.Body>
             </Card>
-            <Button onClick={onDeploy}>onDeploy</Button>
-            <Button onClick={onCheckTxn}>Check Transaction</Button>
-            <Button onClick={onClick}>Call Contract's Function</Button>
-            <Button onClick={checkFunctionCall}>Check If You Won</Button>
-            <pre>
-                functionReturn:
-                {functionReturn
-                    ? 'The return of the function is' + functionReturn
-                    : 'Contract has yet to be called'}
-            </pre>
 
         </div>
     );
