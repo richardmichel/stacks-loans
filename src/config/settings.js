6dc1e5f1fa9d7c24a1864cb138dae8ceb765a5d3
Joyce Lozano <joyce.lozano@onephase.com>
2020-08-24 20:58:43 -0500

    Host port changed

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,11 +1,9 @@
-import { AppConfig } from 'blockstack';
-export const appConfig = new AppConfig(['store_write', 'publish_data']);
-export const STATUS = 'local';
+import { AppConfig } from "blockstack";
+export const appConfig = new AppConfig(["store_write", "publish_data"]);
+export const STATUS = "local";
 export const BASE_URL = {
-    'local': 'http://127.0.0.1:3000/',
-    'dev': 'https://',
-    'production': 'https://',
+  local: "http://127.0.0.1:4000/",
+  dev: "https://",
+  production: "https://",
 };
-export const API_URL = BASE_URL[STATUS] ;
-
-
+export const API_URL = BASE_URL[STATUS];
